2003ee60c0d58c382e760dccad2aa4fee78037e2
Samuel Weibel <samuel.weibel@gmail.com>
2025-09-21 15:47:19 +0000

    Starting to copy js-controller

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,33 @@
 #!/usr/bin/env node
 
-const yargs = require('yargs/yargs');
-const { hideBin } = require('yargs/helpers');
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
 
 interface Arguments {
   verbose?: boolean;
 }
 
 const argv = yargs(hideBin(process.argv))
-  .option('verbose', {
-    alias: 'v',
-    type: 'boolean',
-    description: 'Run with verbose logging',
+  .option("verbose", {
+    alias: "v",
+    type: "boolean",
+    description: "Run with verbose logging",
     default: false,
   })
   .help()
-  .alias('help', 'h')
+  .alias("help", "h")
   .parseSync() as Arguments;
 
 function main(): void {
-  console.log('ioBroker Kubernetes Controller');
-  
+  console.log("ioBroker Kubernetes Controller");
+
   if (argv.verbose) {
-    console.log('Verbose mode enabled');
-    console.log('Arguments:', argv);
+    console.log("Verbose mode enabled");
+    console.log("Arguments:", argv);
   }
-  
+
   // TODO: Implement controller logic
-  console.log('Controller starting...');
+  console.log("Controller starting...");
 }
 
 if (require.main === module) {
